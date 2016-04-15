fc86e6904f9546f74eaa8f44d6f53484c22ca3a3
dyangliew <dyangliew@qk.com.au>
2016-04-15 16:57:47 +1000

    added flex boxes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ document.addEventListener('DOMContentLoaded', function () {
+    particleground(document.getElementById('particles'), {
+        dotColor: '#C0DEFC',
+        lineColor: '#c0c0fc',
+        proximity: 150,
+        density:20000,
+        parallax: 10
+    });
+}, false);
