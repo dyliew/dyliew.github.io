1bdc4a85703e9c8f7954e8c3dc2259858ba140e1
DY Liew <daoyang.liew@gmail.com>
2016-05-14 15:58:55 +1000

    added background image

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
  document.addEventListener('DOMContentLoaded', function () {
 	var params = window.location.search;
 	params.replace("?", '').split('&').forEach(function(item){
-		if (item === 'particle=true'){
-			setTimeout(function(){
-				particleground(document.getElementById('particles'), {
-		        	dotColor: '#C0DEFC',
-			        lineColor: '#c0c0fc',
-			        proximity: 300,
-			        density: 30000
-			    });
-			}, 0);
-		} else {
-			// var wrapper = document.getElementById('wrapper');
-			// if (wrapper) {
-			// 	wrapper.style['border-width'] = '2 0 2 0';
-			// 	wrapper.style['border-style'] = 'dashed';
-			// 	wrapper.style['border-color'] = 'darkslategray';
-			// }
-		}
+		if (item !== 'particle=true')
+			return;
+
+		var background = document.getElementById('background');
+		background.style.display = 'none';
+		setTimeout(function(){
+			particleground(document.getElementById('particles'), {
+	        	dotColor: '#C0DEFC',
+		        lineColor: '#c0c0fc',
+		        proximity: 300,
+		        density: 30000
+		    });
+		}, 0);
 	});
 }, false);
